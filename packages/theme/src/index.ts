843d9066a4b445d672629ed7a46d6252b6eaf97e
CK. <2655257336@qq.com>
2025-12-05 21:25:28 +0800

    feat: 在主题模块中应用默认的 lightTheme，确保在浏览器环境中正确加载主题

--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -2,3 +2,10 @@ export * from "./types";
 export * from "./themes";
 export * from "./applyTheme";
 export * as tokens from "./tokens";
+
+import { lightTheme } from "./themes";
+import { applyTheme } from "./applyTheme";
+
+if (typeof window !== "undefined" && typeof document !== "undefined") {
+  applyTheme(lightTheme);
+}
